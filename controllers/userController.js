110234a2c6ecebaa5df47f01ddf2d42e18587170
sava <savamedvedev@gmail.com>
2022-07-24 18:47:20 +0800

    thour commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -36,10 +36,16 @@ const updateUser = async (req, res) => {
 const deleteUser = async (req, res) => {
   if (req.body.userId === req.params.id) {
     try {
-      await User.findByIdAndDelete(req.params.id);
-      res.status(200).json("User has been deleted...");
-    } catch (error) {
-      res.status(500).json(error);
+      const user = await User.findById(req.params.id);
+      try {
+        await Post.deleteMany({ username: user.username });
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted...");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } catch (err) {
+      res.status(404).json("User not found!");
     }
   } else {
     res.status(401).json("You can delete only your account!");
--- index.js
+++ index.js
@@ -1,24 +1,44 @@
 const express = require("express");
 const app = express();
-const authRoute = require("./routes/auth");
-const userRoute = require("./routes/users");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const chalk = require("chalk");
 const multer = require("multer");
 const path = require("path");
-///////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+/////////////////////////////////////////////////////////////////////
 
 dotenv.config();
 app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log(chalk.bgBlue("Connected to MongoDB")))
   .catch((err) => console.log(err));
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen(process.env.PORT, () => {
   console.log(chalk.bgBlue(`Server started in port: ${process.env.PORT}`));
