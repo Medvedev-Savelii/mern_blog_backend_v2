7a7cbb077bffb160150a9bf05a0c3a1aa94ed57f
sava <savamedvedev@gmail.com>
2022-07-24 12:28:00 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const chalk = require("chalk");
+const multer = require("multer");
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "/images")));
+app.use(express.json());
+dotenv.config();
+
+const start = async () => {
+  try {
+    await mongoose.connect(
+      process.env.MONGO_URL,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: true,
+      },
+      () => {
+        console.log(chalk.bgBlue("DB CONECTION"));
+      }
+    );
+
+    app.listen(process.env.PORT, () => {
+      console.log(chalk.bgBlue(`Server started in port: ${process.env.PORT}`));
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+start();
